510143499f1f2de731ae0fde75324eb5a0f49913
xup6890609 <xup6890609@gmail.com>
2021-01-04 22:47:46 +0800

    建立、關閉對話框

---
+++ 3D_End_107050006/Assets/Scripts/NPC.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;           //引用系統.集合(協同程序)
+
+public class NPC : MonoBehaviour
+{
+    [Header("NPC資料")]
+    public NPCData data;
+    [Header("對話框")]
+    public GameObject dialogue;
+    [Header("對話內容")]
+    public Text textContent;
+    [Header("對話者名稱")]
+    public Text textName;
+    [Header("對話間隔")]
+    public float interval = 0.1f;
+
+    /// <summary>
+    /// 玩家是否進入感應區
+    /// </summary>
+    public bool playerInArea;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "艾爾")
+        {
+            playerInArea = true;
+            StartCoroutine(Dialogue());
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "艾爾")
+        {
+            playerInArea = false;
+        }
+    }
+    /// <summary>
+    /// 停止對話
+    /// </summary>
+    private void StopDialogue() 
+    {
+        dialogue.SetActive(false);
+        StopAllCoroutines();
+    }
+  
+    /// <summary>
+    /// 開啟對話
+    /// </summary>
+    /// StartCoroutine:啟動協程
+    /// <summary>
+    /// IEnumerator:協同程序
+    /// </summary>
+    private IEnumerator Dialogue()
+    {
+        dialogue.SetActive(true);                           //顯示對話框
+        textContent.text = "";                              //清空對話框文字
+        textName.text = name;
+
+
+        for (int i = 0; i < data.dialogueA.Length; i++)     //字串長度       //for迴圈：重複處理相同程式
+        {
+            textContent.text += data.dialogueA[i] + "";     //將文字串聯
+            yield return new WaitForSeconds(interval);
+        }
+    }
+}
---
+++ 3D_End_107050006/Assets/Scripts/NPCData.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// ScriptableObject:腳本化物件→將腳本變成物件保存在專案內
+/// </summary>
+[CreateAssetMenu(fileName ="NPC資料",menuName = "107050006/NPC資料")]         //建立資源選單("檔案名稱,選單名稱")
+public class NPCData : ScriptableObject
+
+{
+    [Header("第一段對話"),TextArea(1,5)]
+    public string dialogueA;
+    [Header("第二段對話"),TextArea(1, 5)]
+    public string dialogueB;
+    [Header("第三段對話"),TextArea(1, 5)]
+    public string dialogueC;
+    [Header("第四段對話"),TextArea(1, 5)]
+    public string dialogueD;
+    [Header("任務要求數量")]
+    public int count;
+    [Header("已完成數量")]
+    public int countCurrent;
+}
